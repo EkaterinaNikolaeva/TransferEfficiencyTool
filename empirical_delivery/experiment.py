ea121310db03c1f3de468ece923ae40887606d00
yekaterinika <yekaterina.nikolaeva2004@yandex.ru>
2024-11-09 01:22:19 +0300

    experimental setup

---
+++ empirical_delivery/experiment.py
@@ -0,0 +1,15 @@
+from casync import Casync
+from desync import Desync
+from rsync import Rsync
+
+
+def preprocess():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
